28b8710b7a8f68b71cc103b8770e1194f281e557
Rishabh Singh <ris8645208@gmail.com>
2025-05-30 23:40:33 +0530

    chore: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import UnoptimizedComponent from "./components/unoptimized-component";
 import ReactUnOptimizedComponent from "./components/unoptimized/App";
 
 const App = () => {
+  // ----------------------------------------------
+  // Just one component can be tested at a time
+  return <UnoptimizedComponent />;
+  // ----------------------------------------------
+  // Multiple components can be tested here
   // return <ReactUnOptimizedComponent />;
-  return <ReactOptimizedComponent />;
-  // return <UnoptimizedComponent />;
+  // return <ReactOptimizedComponent />;
 };
 
 export default App;
